b6b3538fddb5aae4d095e4aaae8417cb26f9e754
HoochDeveloper <michelesalvatore.rillo@gmail.com>
2018-04-09 23:27:18 +0200

    Added sample method for LSTM autoencoder

--- Minerva.py
+++ Minerva.py
@@ -8,6 +8,7 @@ from Demetra import TimeSeriesDataset,TimeSeriesPreprocessing
 
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, TimeDistributed
+from keras.models import load_model
 from numpy import array
 
 #KERAS ENV GPU
@@ -28,69 +29,105 @@ def main():
 	#batteries = tsd.loadEpisodedDataset("dataset",dataFile="episodedDF")
 	trainX, trainY, validX, validY, testX, testY = tsd.dataFormatKerasLSTM("dataset",dataFile="kerasData.gzip",force=False)
 	
+	
+	
+	
+	
+	
+	#if(True):
+	#	i = 1
+	#	plt.figure()
+	#	for col in range(trainX[0].shape[2]):
+	#		plt.subplot(trainX[0].shape[2], 1, i)
+	#		plt.plot(trainX[0][0][:, col])
+	#		i += 1
+	#	plt.show()
+	
+	minerva = Minerva()
+	#minerva.getModel(trainX, trainY, validX, validY, testX, testY)
+	
+	testX = minerva.batchCompatible(300,testX)
+	
+	model = load_model('batteryLSTM.h5')
+	tt = time.clock()
+	Yhat = model.predict(testX[0],batch_size=300)
+	logger.info("Prediction completed. Elapsed %f second(s)" %  (time.clock() - tt))
+	logger.info(len(Yhat))
+	logger.info(Yhat.shape)
+	
 	if(True):
 		i = 1
 		plt.figure()
-		for col in range(trainX[0].shape[2]):
-			plt.subplot(trainX[0].shape[2], 1, i)
-			plt.plot(trainX[0][0][:, col])
+		for col in range(testY[0].shape[2]):
+			plt.subplot(testY[0].shape[2], 1, i)
+			plt.plot(Yhat[0][:, col])
+			plt.plot(testY[0][0][:, col])
 			i += 1
 		plt.show()
 	
-	#minerva = Minerva()
-	#minerva.getModel(train,valid,test)
-	#logger.info("%d Batteries",len(batteries))
-	#logger.info("%d Day(s) for battery 0 ",len(batteries[0]))
-	#logger.info("%d Hour(s) for battery 0 in day 0" ,len(batteries[0][0]))
-	#logger.info(batteries[0][0][0].shape)
-	#
-	#
-	#data = batteries[0][0][0]#.reindex(index=padding)
-	#data.reset_index(drop=True,inplace=True)
-	#padding = range(3600)
-	#data = data.reindex(index=padding)
-	#data[tsd.timeIndex].fillna(pd.to_datetime('1900-01-01T00:00:00'),inplace=True)
-	#data.fillna(0,inplace=True)
-	#logger.info(data.shape)
-	#tsd.plot(data)
-				
 		
 class Minerva():
 	"""
 	Model for learning 
 	"""
-	def getModel(self,train,valid,test):
+	def getModel(self,trainX, trainY, validX, validY, testX, testY):
+		tt = time.clock()
 		# battery, days, X, Y
-		batch_size = 32
+		batch_size = 300
+		epochs = 17
+		# shorten list to multiple of batch_size
+		trainX = self.batchCompatible(batch_size,trainX)
+		trainY = self.batchCompatible(batch_size,trainY)
+		validX = self.batchCompatible(batch_size,validX)
+		validY = self.batchCompatible(batch_size,validY)
+		testX = self.batchCompatible(batch_size,testX)
+		testY = self.batchCompatible(batch_size,testY)
+
+		logger.info(trainX[0].shape)
+		logger.info(validX[0].shape)
+		logger.info(testX[0].shape)
+		
 		hiddenStateDim = 8
-		inputFeatures = train[0][0][0].shape[2]
-		outputFeatures = train[0][0][1].shape[2]
-		timeSteps = train[0][0][0].shape[1]
+		inputFeatures = trainX[0].shape[2]
+		outputFeatures = trainY[0].shape[2]
+		timeSteps = trainX[0].shape[1]
 		model = Sequential()
-		#encoder = LSTM(hiddenStateDim, input_shape=(timeSteps,inputFeatures))
-		encoder = LSTM(hiddenStateDim, stateful=True,batch_input_shape=(batch_size, timesteps, inputFeatures))
+		
+		encoder = (
+		LSTM(hiddenStateDim, 
+		batch_input_shape=(batch_size, timeSteps, inputFeatures),stateful=True,
+		return_sequences=True))
 		
 		model.add(encoder)
 		
-		decoder =  LSTM(outputFeatures,input_shape=(timeSteps,hiddenStateDim), stateful=True)
+		decoder = (
+		LSTM(outputFeatures,
+		input_shape=(timeSteps,hiddenStateDim),
+		return_sequences=True,stateful=True)
+		)
 		model.add(decoder)
 		model.compile(loss='mse', optimizer='adam')
 		
-		batteries = len(train)
-		logger.info("Training %s episodes" % epochs)
+		batteries = len(trainX)
+		logger.info("Training %s episodes" % batteries)
 		for battery in range(batteries):
-			print("Training epoch %s of %s" % ((epoch+1),numOfEpochs),end='\r')
-			
+			print("Training battery %s of %s" % ((battery+1),batteries),end='\r')
 			(
-			model.fit(train[battery][0], train[battery][1],
-			batch_size=batch_size, epochs=25, shuffle=False,
-			validation_data=(valid[battery][0], valid[battery][1]))
+			model.fit(trainX[battery],trainY[battery],
+			batch_size=batch_size, epochs=epochs, shuffle=False,
+			validation_data=(validX[battery], validY[battery]))
 			)
 			model.reset_states()
-			
-			
+		model.save('batteryLSTM.h5')  # creates a HDF5 file 'batteryLSTM.h5'
+		
 		logger.info("Training completed. Elapsed %f second(s)" %  (time.clock() - tt))
 	
+	def batchCompatible(self,batch_size,list):
+		for i in range(len(list)):
+			exceed = list[i].shape[0] % batch_size
+			if(exceed > 0):
+				list[i] = list[i][:-exceed]
+		return list
 	
 main()
 
