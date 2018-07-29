8fe70265f46d1a300352ffa7c112a00b109963b9
HoochDeveloper <michelesalvatore.rillo@gmail.com>
2018-07-29 09:55:51 +0200

    :art: :sparkles: Update synthetic data generation. Now the data are generated indipendently for every episode

--- matlab/generate_all_synthetic_data.m
+++ matlab/generate_all_synthetic_data.m
@@ -2,28 +2,52 @@ function [ ] = generate_all_synthetic_data(batterySOCs )
 %GENERATE_ALL_SYNTHETIC_DATA generate synthetic data for specified initial
 %SOCs
 %   
-    warning off;
-    files = dir('./csv/*.csv');
-    num = size(files);
-    numFiles = num(1);
-    mdl = 'power_battery_edited';
-    for k = 1:length(batterySOCs)
-        batterySOC = num2str(batterySOCs(k));
-        in = Simulink.SimulationInput(mdl);
-        set_param('power_battery_edited/battery','SOC',batterySOC);
-        f = waitbar(0,strcat('Computing soc -> ', batterySOC));
-        for i = 1:numFiles
-            perc = i/numFiles;
-            file = files(i).name;
-            [filepath,batteryName,ext] = fileparts(file);
-            waitbar(perc,f,strcat(batteryName,'@SOC ',batterySOC));
-            [ts,stopTime] = load_real_data(batteryName); % ts is required in simulink model
-            in = in.setVariable('ts',ts);
-            set_param(mdl, 'StopTime',num2str(stopTime) );
-            simOut = sim(in);
-            generate_synthetic_data( batteryName,batterySOC, simOut.voltage.Data, simOut.SOC.data );
-        end
-        close(f);
+    
+    saveRoot = './synthetic_data/';
+    if not(exist(saveRoot, 'dir'))
+        mkdir(saveRoot);
     end
-    warning on;
+    mdl = 'battery_synthetic';
+    in = Simulink.SimulationInput(mdl);
+    root = './exportEpisodes';
+    batteryDirs = dir(root);
+    
+    dirFlags = [batteryDirs.isdir];
+    batteryDirs = batteryDirs(dirFlags);
+    
+    numBatt = size(batteryDirs);
+    numBattery = numBatt(1);
+    f = waitbar(0,'Synthetize batteries');
+    for d = 1:numBattery
+        perc = d/numBattery;
+        
+        batteryName = batteryDirs(d).name;
+        
+        waitbar(perc,f,strcat('Synthetizing ',batteryName));
+        
+        episodesPath = strcat(root,'/',batteryName,'/*.csv');
+        episodes = dir(episodesPath);
+        numEp = size(episodes);
+        numEpisode = numEp(1);
+        
+        for k = 1:length(batterySOCs)
+            episodeSaveFolder = strcat(saveRoot,batteryName,'_',num2str( batterySOCs(k)));
+            if not(exist(episodeSaveFolder, 'dir'))
+                mkdir(episodeSaveFolder);
+            end
+            for e = 1 : numEpisode
+                agedQ = int32(340 * batterySOCs(k) / 100);
+                nomQ2set = num2str(agedQ);
+                episodePath = strcat(root,'/',batteryName);
+                % ts is required in simulink model
+                [ts,stopTime] = load_real_data(episodePath,episodes(e).name); 
+                in = in.setVariable('ts',ts);
+                set_param(mdl, 'StopTime',num2str(stopTime) );
+                set_param( strcat( mdl,'/battery'),'NomQ',nomQ2set);
+                simOut = sim(in);
+                fileForSave = strcat(episodeSaveFolder,'/',episodes(e).name);
+                csvwrite(fileForSave,simOut.voltage.Data);
+            end
+        end
+    end
 end
--- matlab/load_real_data.m
+++ matlab/load_real_data.m
@@ -1,7 +1,7 @@
-function [ts,stopTime] = load_real_data( batteryName )
+function [ts,stopTime] = load_real_data(path,episodeName )
 %LOAD_REAL_DATA Summary of this function goes here
 %   Detailed explanation goes here
-    loaded = csvread(strcat('./csv/' , batteryName  , '.csv'),1,0);
+    loaded = csvread(strcat(path ,'/',episodeName),1,0);
     current = loaded(:,1);
     ts = timeseries(current);
     stopTime = length(current)-1; %simulation starts from 0
