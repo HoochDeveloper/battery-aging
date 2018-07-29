2dc8b6d9c24d3dfa9dc0a6b28bca16241bf0f71e
HoochDeveloper <michelesalvatore.rillo@gmail.com>
2018-07-29 16:58:54 +0200

    :battery: updated synthetic generation

--- matlab/generate_all_synthetic_data.m
+++ matlab/generate_all_synthetic_data.m
@@ -31,23 +31,28 @@ function [ ] = generate_all_synthetic_data(batterySOCs )
         numEpisode = numEp(1);
         
         for k = 1:length(batterySOCs)
+            
+            agedQ = int32(340 * batterySOCs(k) / 100);
+            nomQ2set = num2str(agedQ);
+            set_param( strcat( mdl,'/battery'),'NomQ',nomQ2set);
+         
             episodeSaveFolder = strcat(saveRoot,batteryName,'_',num2str( batterySOCs(k)));
             if not(exist(episodeSaveFolder, 'dir'))
                 mkdir(episodeSaveFolder);
             end
             for e = 1 : numEpisode
-                agedQ = int32(340 * batterySOCs(k) / 100);
-                nomQ2set = num2str(agedQ);
+                
                 episodePath = strcat(root,'/',batteryName);
                 % ts is required in simulink model
                 [ts,stopTime] = load_real_data(episodePath,episodes(e).name); 
                 in = in.setVariable('ts',ts);
                 set_param(mdl, 'StopTime',num2str(stopTime) );
-                set_param( strcat( mdl,'/battery'),'NomQ',nomQ2set);
+                
                 simOut = sim(in);
                 fileForSave = strcat(episodeSaveFolder,'/',episodes(e).name);
                 csvwrite(fileForSave,simOut.voltage.Data);
             end
         end
     end
+    close(f)
 end
